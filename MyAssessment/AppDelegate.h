5d7eb7569e83658ef34fc21a6fcc8fe89747018e
Asfanur <asfanur.arafin@gmail.com>
2015-04-24 15:18:25 +1000

    Initial Commit

---
+++ MyAssessment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyAssessment
+//
+//  Created by Asfanur Arafin on 24/04/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
